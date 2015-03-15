52c0e1367eaaa6bf0f0d72e3577083173785a28d
BernardoB95 <bernardo.bello95@hotmail.com>
2015-03-15 14:31:29 -0430

    Salas
    Se puede Anadir la sala y tambien inclui para quitar la sala.

--- src/LDSalas.java
+++ src/LDSalas.java
@@ -34,24 +34,111 @@ public class LDSalas {
         }
     }
     
-    public void insertarSala(NDSalas nuevo) {
-        if (cabeza==null) {
+    public void InsertarSala(NDSalas nuevo) {
+        if (cabeza==null) 
+        {
             cabeza = cola = nuevo;
-        } else {
-            if(cabeza==cola){
-                nuevo.setProx(cabeza);
-                cola = cabeza;
-                cabeza = nuevo;
-                cola.setAnterior(cabeza);
-            }
-            else{
-                nuevo.setProx(cabeza);
-                cabeza.setAnterior(nuevo);
-                cabeza = nuevo;
-            }   
+        } else 
+        {
+            cola.setProx(nuevo);
+            nuevo.setAnterior(cola);
+            cola = nuevo;
         }
         
 
+    }
+    
+    public NDSalas EliminarPrimero()
+    {
+        NDSalas SalaEliminada = null;
+        if(cabeza == null)
+        {
+            if(cabeza == cola)
+            {
+                SalaEliminada = cabeza;
+                cabeza = cola = null;
+            }
+            else
+            {
+                SalaEliminada = cabeza;
+                cabeza = cabeza.getProx();
+                cabeza.setAnterior(null);
+                SalaEliminada.setProx(null);
+                
+            }
+            
+        }
+        return SalaEliminada;
+    }
+    
+    public NDSalas EliminarFinal()
+    {
+        NDSalas SalaEliminada = null;
+        if(cabeza == null)
+        {
+            if(cabeza == cola)
+            {
+                SalaEliminada = cola;
+                cabeza = cola = null;
+            }
+            else
+            {
+                SalaEliminada = cola;
+                cola = cola.getAnterior();
+                cola.setProx(null);
+                SalaEliminada.setAnterior(null);
+            }
+        }
+        return SalaEliminada;
+    }
+    
+    public int Contar()
+    {
+        NDSalas aux = cabeza;
+        int cont = 0;
+        while(aux!=null)
+        {
+            aux = aux.getProx();
+            cont++;
+        }
+        return cont;
+    }
+    
+    public NDSalas EliminarSala(int sala) //Modificar Sala(Eliminar)
+    {
+        NDSalas SalaEliminada = null;
+        if(cabeza==null)
+        {
+            if(sala ==1)
+            {
+                SalaEliminada = EliminarPrimero();
+            }
+            else if(sala == Contar())
+            {
+                SalaEliminada = EliminarFinal();
+            }
+            else if (sala > Contar()+1)
+            {
+                System.out.println("Error : Sala ingresada inexistente");
+            }
+            else
+            {
+                NDSalas aux = cabeza;
+                int cont = 1;
+                while(cont < sala - 1)
+                {
+                    cont ++;
+                    aux = aux.getProx();
+                }
+                SalaEliminada = aux.getProx();
+                aux.setProx(SalaEliminada.getProx());
+                SalaEliminada.getProx().setAnterior(aux);
+                SalaEliminada.setAnterior(null);
+                SalaEliminada.setProx(null);
+            }
+            
+        }
+        return SalaEliminada;
     }
     
     public void modificar()
